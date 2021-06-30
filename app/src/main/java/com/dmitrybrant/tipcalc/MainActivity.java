c3f6a3718f1ed33c33c0198ff1b3acf6467e1c32
Dmitry Brant <dbrant@wikimedia.org>
2021-06-30 16:43:17 -0400

    Remove Kotlin. (APK size: 3.1 MB)

---
+++ app/src/main/java/com/dmitrybrant/tipcalc/MainActivity.java
@@ -0,0 +1,52 @@
+package com.dmitrybrant.tipcalc;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textPercent15;
+    private TextView textPercent18;
+    private TextView textPercent20;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText amountText = findViewById(R.id.percentEditText);
+        textPercent15 = findViewById(R.id.percentView15);
+        textPercent18 = findViewById(R.id.percentView18);
+        textPercent20 = findViewById(R.id.percentView20);
+
+        amountText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                float amount = 0f;
+                try {
+                    amount = Float.parseFloat(s.toString());
+                } catch (Exception e) {
+                    // ignore
+                    e.printStackTrace();
+                }
+                textPercent15.setText(getString(R.string.percent_text, 15, amount * 0.15f));
+                textPercent18.setText(getString(R.string.percent_text, 18, amount * 0.18f));
+                textPercent20.setText(getString(R.string.percent_text, 20, amount * 0.2f));
+            }
+        });
+
+        amountText.requestFocus();
+    }
+}
